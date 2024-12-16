6556dd2dc5d3a2c15bc53c8f44824e51531b9fa8
EfazDev <me@efaz.dev>
2024-12-15 19:44:25 -0600

    v1.5.0

--- EfazRobloxBootstrapAPI.py
+++ EfazRobloxBootstrapAPI.py
@@ -1,7 +1,7 @@
 # 
 # Efaz's Roblox Bootstrap API
 # Made by Efaz from efaz.dev
-# v1.5.0
+# v1.4.1
 # 
 # Provided to Mod Mode Scripts using variable EfazRobloxBootstrapAPI
 # Developers may use the following line to see the full API in Visual Studio Code:
